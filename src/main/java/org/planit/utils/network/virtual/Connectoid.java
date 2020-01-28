8b381c23c196527e83600ac6cf53378693d39120
Mark Raadsen <mark.raadsen@sydney.edu.au>
2020-01-28 15:39:56 +0100

    added connectoids and connectoid segment interfaces

---
+++ src/main/java/org/planit/utils/network/virtual/Connectoid.java
@@ -0,0 +1,54 @@
+package org.planit.utils.network.virtual;
+
+import org.planit.exceptions.PlanItException;
+import org.planit.utils.network.Edge;
+
+/** the connecting component between centroid and a first physical node in the network.
+ * Note that all connectoids are edges but not all edges are connectoids
+ * 
+ * @author markr
+ *
+ */
+public interface Connectoid extends Edge {
+
+	/**
+	 * Default connectoid length in km
+	 */
+	double DEFAULT_LENGTH_KM = 1.0;
+
+	/**
+	 * Register connectoidSegment.
+	 * 
+	 * If there already exists a connectoidSegment for that direction it is replaced
+	 * and returned
+	 * 
+	 * @param connectoidSegment
+	 *            connectoid segment to be registered
+	 * @param directionAB
+	 *            direction of travel
+	 * @return replaced ConnectoidSegment
+	 * @throws PlanItException
+	 *             thrown if there is an error
+	 */
+	ConnectoidSegment registerConnectoidSegment(ConnectoidSegment connectoidSegment, boolean directionAB)
+			throws PlanItException;
+
+	/**
+	 * 
+	 * Return the unique id of this connectoid
+	 * 
+	 * @return id of this connectoid
+	 */
+	long getConnectoidId();
+
+	/** Collect the external id of this connectoid
+	 * @return external id
+	 */
+	long getExternalId();
+
+	/** set the external id of this connectoid
+	 * @param externalId
+	 */
+	void setExternalId(long externalId);
+
+}
---
+++ src/main/java/org/planit/utils/network/virtual/ConnectoidSegment.java
@@ -0,0 +1,18 @@
+package org.planit.utils.network.virtual;
+
+import org.planit.utils.network.EdgeSegment;
+
+/** Connectoid segment represents a directional virtual segment connecting a centroid and a physical node.
+ * It has a unique id across all connectoid segments
+ * 
+ * @author markr
+ *
+ */
+public interface ConnectoidSegment extends EdgeSegment{
+
+	/** Collect the unqiue connectoid segment id
+	 * @return
+	 */
+	int getConnectoidSegmentId();
+
+}
