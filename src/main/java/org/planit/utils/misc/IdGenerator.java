7fe931f65f788e649c6194e0f3de8d3d48b23bae
Mark Raadsen <mark.raadsen@sydney.edu.au>
2020-07-24 18:20:38 +1000

    - working on fixing issue #24. NOT DONE

--- src/main/java/org/planit/utils/misc/IdGenerator.java
+++ src/main/java/org/planit/utils/misc/IdGenerator.java
@@ -1,35 +1,56 @@
 package org.planit.utils.misc;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Convenience class to track unique ids across different classes that decide to
  * use a generator for their id members. Ids are generated starting at zero and in an incremental
- * consecutive fashion
- * allowing the placement of objects in raw arrays and indexing them by their id
+ * consecutive fashion. They are unique to their parent though, so if the same class is registered twice with different parents, then their ids will be unique per parent
+ * but not across parents.
  * 
  * @author markr
  *
  */
 public final class IdGenerator {
-
-  /** track unique id's per specific class */
-  private static HashMap<Class<? extends Object>, Integer> idTypes = new HashMap<Class<? extends Object>, Integer>();
-
+  
+  /** track unique id's per object and specific class */
+  private static Map<Object, LocalIdGenerator> idObjects = new HashMap<Object,LocalIdGenerator>();
+  
+  /** track unique id's for classes that do not have a parent */
+  private static LocalIdGenerator idGenerator = new LocalIdGenerator();
+  
   /**
-   * Create a new idGenerator for this type such that we track unique id's within
-   * this class
+   * Create new idGenerators for this object such that we track unique id's within
+   * this object
    * 
+   * @param parent the parent object for which ids will be generated
+   * @return created IdGenerator
+   */
+  protected static LocalIdGenerator createIdGeneratorForParent(Object parent) {
+    LocalIdGenerator idGenerator = new LocalIdGenerator();
+    idObjects.put(parent, idGenerator);
+    return idObjects.get(parent);
+  }  
+  
+  /**
+   * Generate a unique id for the chosen class
+   * 
+   * @param parent the parent for which the id is related to 
    * @param theClass
    *          the class for which the id is being generated
+   * @return the generated id
    */
-  protected static void createNewIdType(Class<? extends Object> theClass) {
-    Integer initialId = 0; // choose 0 as this way we can use each id as an index in an array
-                           // without
-                           // additional effort
-    idTypes.put(theClass, initialId);
+  public static int generateId(Object parent, Class<? extends Object> theClass) {
+    LocalIdGenerator idGeneratorForParent = null;
+    if (!idObjects.containsKey(parent)) {
+      idGeneratorForParent = createIdGeneratorForParent(parent);
+    } else {
+      idGeneratorForParent = idObjects.get(parent);
+    }
+    return idGeneratorForParent.generateId(theClass);
   }
-
+  
   /**
    * Generate a unique id for the chosen class
    * 
@@ -38,20 +59,14 @@ public final class IdGenerator {
    * @return the generated id
    */
   public static int generateId(Class<? extends Object> theClass) {
-    if (!idTypes.containsKey(theClass)) {
-      createNewIdType(theClass);
-    } else {
-      int id = idTypes.get(theClass);
-      id++;
-      idTypes.put(theClass, id);
-    }
-    return idTypes.get(theClass);
-  }
+    return idGenerator.generateId(theClass);
+  }  
 
   /**
    * Reset the id generation at the start of a run
    */
   public static void reset() {
-    idTypes = new HashMap<Class<? extends Object>, Integer>();
+    idObjects.clear();
+    idGenerator.reset();
   }
 }
---
+++ src/main/java/org/planit/utils/misc/LocalIdGenerator.java
@@ -0,0 +1,57 @@
+package org.planit.utils.misc;
+
+import java.util.HashMap;
+
+/**
+ * Convenience class to track unique ids across different classes that decide to
+ * use a generator for their id members. Ids are generated starting at zero and in an incremental
+ * consecutive fashion
+ * allowing the placement of objects in raw arrays and indexing them by their id
+ * 
+ * @author markr
+ *
+ */
+final class LocalIdGenerator {
+
+  /** track unique id's per specific class */
+  private HashMap<Class<? extends Object>, Integer> idTypes = new HashMap<Class<? extends Object>, Integer>();
+
+  /**
+   * Create a new idGenerator for this type such that we track unique id's within
+   * this class
+   * 
+   * @param theClass
+   *          the class for which the id is being generated
+   */
+  protected void createNewIdType(Class<? extends Object> theClass) {
+    Integer initialId = 0; // choose 0 as this way we can use each id as an index in an array
+                           // without
+                           // additional effort
+    idTypes.put(theClass, initialId);
+  }
+
+  /**
+   * Generate a unique id for the chosen class
+   * 
+   * @param theClass
+   *          the class for which the id is being generated
+   * @return the generated id
+   */
+  public int generateId(Class<? extends Object> theClass) {
+    if (!idTypes.containsKey(theClass)) {
+      createNewIdType(theClass);
+    } else {
+      int id = idTypes.get(theClass);
+      id++;
+      idTypes.put(theClass, id);
+    }
+    return idTypes.get(theClass);
+  }
+
+  /**
+   * Reset the id generation at the start of a run
+   */
+  public void reset() {
+    idTypes.clear();
+  }
+}
