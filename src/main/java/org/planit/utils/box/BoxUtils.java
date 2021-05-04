be168a043b6f26440e4977063dd2765d1c87dae4
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2021-05-04 10:59:08 +1000

    - new boxing class for utilities to convert between types that often can be auto-boxed (but not always)

---
+++ src/main/java/org/planit/utils/box/BoxUtils.java
@@ -0,0 +1,21 @@
+package org.planit.utils.box;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Utility class assisting in more complex (auto-)boxing of types in for example container classes
+ * @author markr
+ *
+ */
+public class BoxUtils {
+
+  
+  /** Convert an integer list to a long list
+   * @param integerList
+   * @return
+   */
+  public static List<Long> IntegerToLongList(List<Integer> integerList) {
+    return integerList.stream().mapToLong(Integer::longValue).boxed().collect(Collectors.toList());
+  }
+}
