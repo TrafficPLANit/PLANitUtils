1a28daea8c5243ddfd1f0aa343b5ca0538208346
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2021-05-11 15:40:28 +1000

    - Change in creating CRS instances, created new util class for it

---
+++ src/main/java/org/planit/utils/geo/PlanitCrsUtils.java
@@ -0,0 +1,46 @@
+package org.planit.utils.geo;
+
+import java.util.logging.Logger;
+
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+/** Utilities for coordinate Reference systems
+ * 
+ * @author markr
+ *
+ */
+public class PlanitCrsUtils {
+  
+  /** the logger to use */
+  private static final Logger LOGGER = Logger.getLogger(PlanitCrsUtils.class.getCanonicalName());
+
+  /**
+   * create a coordinate reference system instance based on String representation, e.g. "EPSG:4326" for WGS84", using the underlying geotools hsql authority factory. see also
+   * {@code https://docs.geotools.org/latest/userguide/library/referencing/crs.html} on some context on why we include the hsql dependency in the planit build to ensure that the
+   * provided crs codes here can actually be transformed into a viable CRS and why it makes sense to provide this simple wrapper method in this utility class
+   * <p>
+   * always make sure you lookup the CRS via this method as it ensures the logging of PLANit is not messed up by the geotools-HSQL dependency since we programmatically disallow it
+   * to overwrite our logging configuration in the static initialiser of this class.
+   * </p>
+   * 
+   * @param code for the CRS
+   * @return the created coordinate reference system
+   */
+  public static CoordinateReferenceSystem createCoordinateReferenceSystem(String code) {
+    CoordinateReferenceSystem crs = null;
+    if (code != null) {
+      try {
+        /* decode lookup is performed using the gt hsql database which is loaded as dependency in pom */
+        crs = CRS.decode(code);
+      } catch (Exception e1) {
+        try {
+          crs = CRS.decode(code, true);
+        } catch (Exception e2) {
+          LOGGER.warning(String.format("unable to find coordinate reference system for %s", code));
+        }
+      }
+    }
+    return crs;
+  }
+}
