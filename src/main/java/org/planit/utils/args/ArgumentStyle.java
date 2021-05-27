d5b118fbe9e5af8797fc75120b672849bb22b330
Mark Raadsen <mark.raadsen@sydney.edu.au>
2021-05-27 14:12:06 +1000

    - improved resource parsing to deal with resources and uris - argument parser improvements

---
+++ src/main/java/org/planit/utils/args/ArgumentStyle.java
@@ -0,0 +1,13 @@
+package org.planit.utils.args;
+
+/**
+ * Supported styles for argument parsing
+ * 
+ * @author markr
+ *
+ */
+public enum ArgumentStyle {
+
+	DEFAULT,					//"<key>=/-/:<value>"
+	DOUBLEHYPHEN;				//"--<key> <value>"
+}
--- src/main/java/org/planit/utils/resource/ResourceUtils.java
+++ src/main/java/org/planit/utils/resource/ResourceUtils.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.logging.Logger;
 
@@ -42,7 +43,12 @@ public class ResourceUtils {
    * @throws URISyntaxException thrown if error
    */
   public static URI getResourceUri(String resourceLocation) throws URISyntaxException{
-    return getResourceUrl(resourceLocation).toURI();
+    URL url = getResourceUrl(resourceLocation);
+    if(url==null) {
+      LOGGER.warning(String.format("Unable to create resource URL and therefore URI for %s",resourceLocation));
+      return null;
+    }
+    return url.toURI(); 
   }  
 
   /** check if URI is contained within a JAR file
@@ -82,32 +88,67 @@ public class ResourceUtils {
   
   /** Collect a resource input stream reader, which is either file based or not depending whether or not the resource is located in a  jar or not
    * 
-   * @param resourceLocation to use  
    * @param uri to deduce whether or not this resource resides within a jar or not, which drives the type of input reader that is to be
    * created, if null we attempt to create it using the resourceLocation
    * @return input stream reader created, or null if something happened
    * @throws URISyntaxException thrown if error
    */  
-  public static InputStreamReader getResourceAsInputStreamReader(Path resourceLocation, final URI uri) {
-    try {
-      URI localUri = (uri==null) ? getResourceUri(resourceLocation.toString()) : uri;
-      if(localUri==null) {
-        return null;
-      }
-      
+  public static InputStreamReader getResourceAsInputStreamReader(final URI uri) {
+    try {      
       /* depending on whether or not locaton is in a jar we create the input reader differently */
-      if(isResourceInJar(localUri)) {
+      if(isResourceInJar(uri)) {
         /* input stream */
-        return new InputStreamReader(getResourceAsStream(resourceLocation.toString()));
+        return new InputStreamReader(getResourceAsInputStream(uri));
       }else {
         /* file based */
-        return new InputStreamReader(new FileInputStream(resourceLocation.toFile()), "UTF-8");
+        return new InputStreamReader(getResourceAsInputStream(uri), "UTF-8");
       }
     }catch(Exception e) {
       LOGGER.warning(e.getMessage());
-      LOGGER.warning(String.format("Unable to create an input stream reader for resource %s",resourceLocation.toString()));
+      LOGGER.warning(String.format("Unable to create an input stream reader for resource %s",uri.toString()));
     }
     return null;
+  }  
+  
+  /** Collect a resource input stream, which is either file based or not depending whether or not the resource is located in a  jar or not
+   * 
+   * @param uri to deduce whether or not this resource resides within a jar or not, which drives the type of input reader that is to be
+   * created, if null we attempt to create it using the resourceLocation
+   * @return input stream reader created, or null if something happened
+   * @throws URISyntaxException thrown if error
+   */  
+  public static InputStream getResourceAsInputStream(final URI uri) {
+    try {
+      
+      URI thisUri = Path.of("").toUri();
+      /* extract relative location to cwd (if possible), so we can provide a location relative to this */     
+      URI relativeResourceUri = thisUri.relativize(uri);
+      
+      Path relativeResourcePath = null;
+      if(relativeResourceUri.getScheme()!=null) {
+        /* scheme still present, meaning that relative path could not be extracted (because uri is not somewhere in the cwd hierarchy) and therefore the
+         * beginning of uri is kept (which contains the scheme). In that case we extract a path directly from the URI, otherwise the 
+         * relativisation already removed the scheme (at the front) and is effectively a relative path already and no additional effort is needed (which would break
+         * the call) and we simply collect the string representation
+         */
+        relativeResourcePath = Paths.get(relativeResourceUri);
+      }else {
+        relativeResourcePath = Path.of(relativeResourceUri.toString());
+      }
+        
+      /* depending on whether or not locaton is in a jar we create the input reader differently */
+      if(isResourceInJar(uri)) {
+        /* input stream */
+        return getResourceAsStream(relativeResourcePath.toString());
+      }else {
+        /* file based */
+        return new FileInputStream(relativeResourcePath.toFile());
+      }
+    }catch(Exception e) {
+      LOGGER.warning(e.getMessage());
+      LOGGER.warning(String.format("Unable to create an input stream reader for resource %s",uri.toString()));
+    }
+      return null;
   }  
   
   
