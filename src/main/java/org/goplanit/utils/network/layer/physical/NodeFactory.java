f642ab8be8022599dc945a3ed9decbc3031134b8
Mark Raadsen <mark.raadsen@sydney.edu.au>
2022-11-11 16:59:56 +1100

    added new factory methods

--- src/main/java/org/goplanit/utils/network/layer/physical/NodeFactory.java
+++ src/main/java/org/goplanit/utils/network/layer/physical/NodeFactory.java
@@ -1,6 +1,7 @@
 package org.goplanit.utils.network.layer.physical;
 
 import org.goplanit.utils.graph.GraphEntityFactory;
+import org.locationtech.jts.geom.Point;
 
 /** Factory interface for creating node instances
  * 
@@ -14,12 +15,35 @@ public interface NodeFactory extends GraphEntityFactory<Node> {
    * @return created node
    */
   public abstract Node createNew();
+
+  /**
+   * Create a new node (without registering) and set its gemoetry and potentially sync its XML id to the underlying internal id
+   *
+   * @param position to use
+   * @param syncXmlIdToId when true set XML id to internal id, when false leave it null
+   * @return created node
+   */
+  public default Node createNew(Point position, boolean syncXmlIdToId){
+    var node = createNew();
+    node.setXmlId(node.getId());
+    node.setPosition(position);
+    return node;
+  }
   
   /**
    * Create and register new node
    *
    * @return new node created
    */
-  public abstract Node registerNew();      
-  
+  public abstract Node registerNew();
+
+  /**
+   * Register a new node and set its geometry and potentially sync its XML id to the underlying internal id
+   *
+   * @param position to use
+   * @param syncXmlIdToId when true set XML id to internal id, when false leave it null
+   * @return created and registered node
+   */
+  public abstract Node registerNew(Point position, boolean syncXmlIdToId);
+
 }
