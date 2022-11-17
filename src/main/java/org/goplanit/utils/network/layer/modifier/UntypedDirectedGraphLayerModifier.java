bdaf0f6fa976da0532673a7b51820d9fbd21ae3b
Mark Raadsen <mark.raadsen@sydney.edu.au>
2022-11-17 18:03:46 +1100

    added functionality to aid connectoid creation and indexing. To help with the handler when modifying the network (break links and listeners involved)

--- src/main/java/org/goplanit/utils/network/layer/modifier/UntypedDirectedGraphLayerModifier.java
+++ src/main/java/org/goplanit/utils/network/layer/modifier/UntypedDirectedGraphLayerModifier.java
@@ -1,15 +1,20 @@
 package org.goplanit.utils.network.layer.modifier;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.goplanit.utils.exceptions.PlanItException;
+import org.goplanit.utils.exceptions.PlanItRunTimeException;
 import org.goplanit.utils.graph.directed.DirectedEdge;
 import org.goplanit.utils.graph.directed.DirectedVertex;
 import org.goplanit.utils.graph.directed.EdgeSegment;
 import org.goplanit.utils.graph.modifier.event.GraphModifierEventProducer;
 import org.goplanit.utils.misc.Pair;
+import org.goplanit.utils.network.layer.MacroscopicNetworkLayer;
 import org.goplanit.utils.network.layer.UntypedDirectedGraphLayer;
+import org.goplanit.utils.network.layer.macroscopic.MacroscopicLink;
+import org.goplanit.utils.network.layer.physical.Node;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
@@ -31,27 +36,9 @@ public interface UntypedDirectedGraphLayerModifier<V extends DirectedVertex, E e
    * @param nodeToBreakAt      the node to break at
    * @param crs                to use to recompute link lengths of broken links
    * @return the broken edges for each original edge's id
-   * @throws PlanItException thrown if error
    */
-  public default Map<Long, Pair<E,E>> breakAt(E linkToBreak, V nodeToBreakAt, CoordinateReferenceSystem crs) throws PlanItException {
+  public default Map<Long, Pair<E,E>> breakAt(E linkToBreak, V nodeToBreakAt, CoordinateReferenceSystem crs) {
     return breakAt(List.of(linkToBreak), nodeToBreakAt, crs);
-  }
-
-  /**
-   * Break the passed in links by inserting the passed in node in between. After completion the original links remain as (NodeA,NodeToBreakAt), and new links as inserted for
-   * (NodeToBreakAt,NodeB). It is assumed no transfer zones exist in the network, otherwise one should use the same method yet provide the zoning as an additiona parameter to
-   * ensure affected connectoids are updated to reflect the new situation
-   * 
-   * Underlying link segments (if any) are also updated accordingly in the same manner
-   * 
-   * @param linksToBreak  the links to break
-   * @param nodeToBreakAt the node to break at
-   * @param crs           to use to recompute link lengths of broken links
-   * @return the broken edges for each original edge's id
-   * @throws PlanItException thrown if error
-   */
-  public default Map<Long, Pair<E,E>> breakLinksAt(List<E> linksToBreak, V nodeToBreakAt, CoordinateReferenceSystem crs) throws PlanItException {
-    return breakAt(linksToBreak, nodeToBreakAt, crs);
   }
 
   /**
@@ -66,5 +53,46 @@ public interface UntypedDirectedGraphLayerModifier<V extends DirectedVertex, E e
    * @return the broken edges for each original edge's id
    */
   public abstract Map<Long, Pair<E,E>> breakAt(List<E> linksToBreak, V nodeToBreakAt, CoordinateReferenceSystem crs);
+
+  /**
+   * Break the passed in links by inserting the passed in node in between. After completion the original links remain as (NodeA,NodeToBreakAt), and new links as inserted for
+   * (NodeToBreakAt,NodeB).
+   *
+   * Underlying link segments (if any) are also updated accordingly in the same manner.
+   *
+   * @param linksToBreak links that will be broken if they contain theNode as an internal geo location
+   * @param nodeToBreakAt      the node to break at
+   * @param crs of the network layer
+   * @param linkToKey mapping results in group by key for result map
+   * @return newly created PLANit links by a custom key obtained from original broken link and each entry containing all broken links for all original link's mapping to the given key value
+   *
+   */
+  public default <K> Map<K, Set<E>> breakAt(
+      List<E> linksToBreak, V nodeToBreakAt, CoordinateReferenceSystem crs, Function<E,K> linkToKey){
+    Map<K, Set<E>> groupNewLinksByOriginalLinkKey = new HashMap<>();
+
+    if(linksToBreak != null) {
+
+      try {
+        /* performing breaking of links at the node given, returns the broken links by the original link's PLANit edge id */
+        Map<Long, Pair<E,E>> localBrokenLinks = breakAt(linksToBreak, nodeToBreakAt, crs);
+        /* add newly created links to the mapping from original broken link to a key, e.g. external id, that together form this entire original link*/
+        if(localBrokenLinks != null) {
+          localBrokenLinks.forEach((id, links) -> {
+            List.of(links.first(),links.second()).forEach( brokenLink -> {
+              final K brokenLinkKey = linkToKey.apply(brokenLink);
+              groupNewLinksByOriginalLinkKey.putIfAbsent(brokenLinkKey, new HashSet<>());
+              groupNewLinksByOriginalLinkKey.get(brokenLinkKey).add(brokenLink);
+            });
+          });
+        }
+      }catch(PlanItRunTimeException e) {
+        throw new PlanItRunTimeException("Unable to break links %s for node %s, something unexpected went wrong",e,
+            linksToBreak.stream().map( link -> link.getExternalId()).collect(Collectors.toSet()).toString(), nodeToBreakAt.getExternalId());
+      }
+    }
+
+    return groupNewLinksByOriginalLinkKey;
+  }
   
 }
--- src/main/java/org/goplanit/utils/zoning/DirectedConnectoids.java
+++ src/main/java/org/goplanit/utils/zoning/DirectedConnectoids.java
@@ -1,6 +1,16 @@
 package org.goplanit.utils.zoning;
 
 
+import org.goplanit.utils.network.layer.physical.UntypedPhysicalLayer;
+import org.goplanit.utils.network.layers.UntypedPhysicalNetworkLayers;
+import org.locationtech.jts.geom.Point;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
 /**
  * container and factory class for directed connectoids
  * 
@@ -8,6 +18,8 @@ package org.goplanit.utils.zoning;
  *
  */
 public interface DirectedConnectoids extends Connectoids<DirectedConnectoid> {
+
+  public static final Logger LOGGER = Logger.getLogger(DirectedConnectoids.class.getCanonicalName());
 
   /**
    * {@inheritDoc}
@@ -20,5 +32,30 @@ public interface DirectedConnectoids extends Connectoids<DirectedConnectoid> {
    */
   @Override
   public abstract DirectedConnectoids clone();
+
+  /**
+   * For a given physical network layers container that has a relatino with these directed connectoids, index all connectoids by their access node location to the physical network on the layers they
+   * connect to. Do this by location rather than node, so it can be used when modifying the network and updating the node the connectoids attach to for example
+   *
+   * @param networkLayers to index for
+   * @return directed connectoids indexed by access node location per layer
+   */
+  public default <L extends UntypedPhysicalLayer<?,?,?>> Map<L, Map<Point, List<DirectedConnectoid>>> indexByPhysicalLayerAndLocation(UntypedPhysicalNetworkLayers<L> networkLayers){
+    Map<L,Map<Point, List<DirectedConnectoid>>> directedConnectoidsByLocation = new HashMap<>();
+    for(var dirConnectoid : this){
+      var accessNode = dirConnectoid.getAccessNode();
+      var accessLinkSegment = dirConnectoid.getAccessLinkSegment();
+      var layer = networkLayers.findFirst(l -> l.getLinkSegments().get(accessLinkSegment.getId())!=null);
+      if(layer == null){
+        continue;
+      }
+
+      directedConnectoidsByLocation.putIfAbsent(layer, new HashMap<>());
+      var layerConnectoids = directedConnectoidsByLocation.get(layer);
+      layerConnectoids.putIfAbsent(accessNode.getPosition(),new ArrayList<>(1));
+      layerConnectoids.get(accessNode.getPosition()).add(dirConnectoid);
+    }
+    return directedConnectoidsByLocation;
+  }
   
 }
