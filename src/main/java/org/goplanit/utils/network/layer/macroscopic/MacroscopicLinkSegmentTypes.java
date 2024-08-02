fcaefba3501d6d092314930721ca1e235f916aa9
markr <mark.raadsen@sydney.edu.au>
2024-08-02 18:17:25 +1000

    added functionality to access group properties

--- src/main/java/org/goplanit/utils/network/layer/macroscopic/MacroscopicLinkSegmentTypes.java
+++ src/main/java/org/goplanit/utils/network/layer/macroscopic/MacroscopicLinkSegmentTypes.java
@@ -1,8 +1,11 @@
 package org.goplanit.utils.network.layer.macroscopic;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.goplanit.utils.id.ManagedIdEntities;
+import org.goplanit.utils.misc.CollectionUtils;
 import org.goplanit.utils.mode.Mode;
 
+import java.util.*;
 import java.util.function.BiConsumer;
 
 /**
@@ -45,6 +48,38 @@ public interface MacroscopicLinkSegmentTypes extends ManagedIdEntities<Macroscop
       }
     }
     return maxSpeedLimitKmH;
+  }
+
+  /**
+   * Construct a list of entries that are functionallu identical (if any), so excluding name, and id fields
+   * but considering all other fields with meaning
+   *
+   * @return list of functional duplicates, empty if none found
+   */
+  public default List<SortedSet<MacroscopicLinkSegmentType>> findFunctionalDuplicates(){
+    List<SortedSet<MacroscopicLinkSegmentType>> duplicates = new ArrayList<>(2);
+    Set<MacroscopicLinkSegmentType> skipSet = new HashSet<>();
+    for(var ls1 : this) {
+      skipSet.add(ls1);
+      SortedSet<MacroscopicLinkSegmentType> duplicatesForLs1 = null;
+      for(var ls2 : this) {
+        if(skipSet.contains(ls2)) continue;
+
+        boolean isEqual = Objects.equals(ls1.getExplicitCapacityPerLaneOrDefault(), ls2.getExplicitCapacityPerLaneOrDefault()) &&
+          Objects.equals(ls1.getExplicitMaximumDensityPerLaneOrDefault(), ls2.getExplicitMaximumDensityPerLaneOrDefault()) &&
+          Objects.equals(ls1.getAllowedModes(), ls2.getAllowedModes()) &&
+          ls1.getAllowedModes().stream().allMatch( m -> Objects.equals(ls1.getAccessProperties(m), ls2.getAccessProperties(m)));
+        if(isEqual){
+          duplicatesForLs1 = duplicates.stream().filter(e -> e.contains(ls1)).findFirst().orElseGet(TreeSet::new);
+          if(duplicatesForLs1.isEmpty()){
+            duplicates.add(duplicatesForLs1);
+          }
+          duplicatesForLs1.add(ls1);
+          duplicatesForLs1.add(ls2);
+        }
+      }
+    }
+    return duplicates;
   }
 
   /**
