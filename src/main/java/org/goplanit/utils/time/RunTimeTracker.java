a14aee3c21b11bd851d4f57b0e0ff867e63bf517
markr <mark.raadsen@sydney.edu.au>
2024-08-15 16:41:35 +1000

    general run time tracker utility added

---
+++ src/main/java/org/goplanit/utils/time/RunTimeTracker.java
@@ -0,0 +1,71 @@
+package org.goplanit.utils.time;
+
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * Track run times for a single entity in total and per iteration
+ */
+public class RunTimeTracker {
+
+  private final String name;
+
+  private final LongAdder totalRunTime = new LongAdder();
+
+  private final LongAdder iterationRunTime = new LongAdder();
+
+  protected RunTimeTracker(String name){
+    this.name = name;
+  }
+
+  protected RunTimeTracker(RunTimeTracker other){
+    this.name = other.name;
+    totalRunTime.add(other.totalRunTime.longValue());
+    iterationRunTime.add(other.iterationRunTime.longValue());
+  }
+
+  /**
+   * Factory method
+   *
+   * @param name to use for tracker
+   * @return new tracker
+   */
+  public static RunTimeTracker of(String name) {
+    return new RunTimeTracker(name);
+  }
+
+  public void addTimeInMillis(long timeInMillis){
+    totalRunTime.add(timeInMillis);
+    iterationRunTime.add(timeInMillis);
+  }
+
+  public long getIterationTimeInMillis(){
+    return iterationRunTime.longValue();
+  }
+
+  public long getTotalTimeInMillis(){
+    return totalRunTime.longValue();
+  }
+
+  public void resetIterationTime(){
+    iterationRunTime.reset();
+  }
+
+  public void reset() {
+    resetIterationTime();
+    totalRunTime.reset();
+  }
+
+  /**
+   * perform shallow clone
+   */
+  public RunTimeTracker shallowClone() {
+    return new RunTimeTracker(this);
+  }
+
+  /**
+   * perform deep clone
+   */
+  public RunTimeTracker deepClone() {
+    return shallowClone();
+  }
+}
---
+++ src/main/java/org/goplanit/utils/time/RunTimesTracker.java
@@ -0,0 +1,78 @@
+package org.goplanit.utils.time;
+
+import java.util.TreeMap;
+
+/**
+ * Convenience class for tracking run times by category and by iteration
+ */
+public class RunTimesTracker {
+
+  private TreeMap<String, RunTimeTracker> trackerByType = new TreeMap<>();
+
+  /**
+   * Default constructor
+   */
+  protected RunTimesTracker(){
+
+  }
+
+  /**
+   * Copy  constructor
+   *
+   * @param other to copy
+   * @param deepCopy flag
+   */
+  protected RunTimesTracker(RunTimesTracker other, boolean deepCopy){
+    if(!deepCopy){
+      trackerByType.putAll(other.trackerByType);
+    }else{
+      other.trackerByType.forEach( (k,v) -> trackerByType.put( k, v.deepClone()));
+    }
+  }
+
+  /** suggested type for tracking run times - general */
+  public static final String GENERAL = "general";
+
+  public RunTimeTracker registerNew(String type){
+    var newTracker = RunTimeTracker.of(type);
+    trackerByType.put(type, newTracker);
+    return newTracker;
+  }
+
+  public RunTimeTracker get(String type){
+    return trackerByType.get(type);
+  }
+
+  public void addTime(String type, long millis){
+    trackerByType.get(type).addTimeInMillis(millis);
+  }
+
+  public void reset(){
+    trackerByType.forEach( (k,v) -> reset(k));
+  }
+
+  public void reset(String type){
+    var entry = trackerByType.get(type);
+    if(entry != null){
+      entry.reset();
+    }
+  }
+
+  public static RunTimesTracker create(){
+    return new RunTimesTracker();
+  }
+
+  /**
+   * perform shallow clone
+   */
+  public RunTimesTracker shallowClone() {
+    return new RunTimesTracker(this, false);
+  }
+
+  /**
+   * perform deep clone
+   */
+  public RunTimesTracker deepClone() {
+    return  new RunTimesTracker(this, true);
+  }
+}
