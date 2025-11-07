8f8c9c7377c76212be50950d6eeacb32ed2655e2
markr <mark.raadsen@sydney.edu.au>
2025-11-07 14:06:54 +1100

    added tri predicate interface

---
+++ src/main/java/org/goplanit/utils/functionalinterface/TriPredicate.java
@@ -0,0 +1,23 @@
+package org.goplanit.utils.functionalinterface;
+
+ /** Predicate Interface which can process three input objects and produce an output
+  * There is no equivalent functional interface in the java.util.function library yet
+  *
+  * @author gman6028
+  *
+  * @param <T> first object to be processed
+  * @param <U> second object to be processed
+  * @param <U> third object to be processed
+  */
+@FunctionalInterface
+interface TriPredicate<T, U, V> {
+
+  /**
+   * Performs this operation on the given arguments
+   *
+   * @param t first object to be processed
+   * @param u second object to be processed
+   * @param v third object to be processed
+   */
+  public abstract boolean test(T t, U u, V v);
+}
