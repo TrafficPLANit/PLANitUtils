691ce069e6cf8f255f08812eaaa57224fc9e9055
markr <mark.raadsen@sydney.edu.au>
2025-12-04 12:04:15 +1100

    minor refactor

--- src/main/java/org/goplanit/utils/functionalinterface/TriPredicate.java
+++ src/main/java/org/goplanit/utils/functionalinterface/TriPredicate.java
@@ -18,6 +18,7 @@ public interface TriPredicate<T, U, V> {
    * @param t first object to be processed
    * @param u second object to be processed
    * @param v third object to be processed
+   * @return result of test
    */
   public abstract boolean test(T t, U u, V v);
 }
